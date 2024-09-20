26ab79cf153321e15dacf7fa9c69fbf41c14f4c3
Minghong Xia <152064456+mhx83@users.noreply.github.com>
2024-09-19 17:44:24 -0700

    Update index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,5 +1,5 @@
 import Lab1 from "./Lab1";
-import { Route, Routes, Navigate } from "react-router";
+import { Routes, Route, Navigate, Link } from 'react-router-dom';
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
@@ -9,6 +9,7 @@ export default function Labs() {
     <div id="wd-labs">
       <h1>Labs</h1>
       <TOC />
+      <Link to="/">Back to Landing Page</Link>
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
