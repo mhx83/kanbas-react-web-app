1d88163979807d603981b555e52bd1daf4ddf37d
Minghong Xia <152064456+mhx83@users.noreply.github.com>
2024-10-03 20:50:55 -0700

    Update index.tsx

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
                 <Modules />
             </div>
 
-            <div className="d-none d-md-block">
+            <div className="d-none d-md-block ms-4">
                 <CourseStatus />
             </div>
         </div>
