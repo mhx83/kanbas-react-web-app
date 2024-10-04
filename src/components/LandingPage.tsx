488a04e4862fc757af79a27da9c8eeda2f7ac3dc
Minghong Xia <152064456+mhx83@users.noreply.github.com>
2024-10-03 17:02:45 -0700

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -7,6 +7,9 @@ function LandingPage() {
             <h1>Minghong Xia - Section #01</h1>
             <ul>
                 <li><Link to="/Labs">Labs</Link></li>
+                <li><Link to="/Labs/Lab1">Lab 1</Link></li>
+                <li><Link to="/Labs/Lab2">Lab 2</Link></li>
+                <li><Link to="/Labs/Lab3">Lab 3</Link></li>
                 <li><Link to="/kanbas">Kanbas Application</Link></li>
             </ul>
             <p>Find all the source code here: <a href="https://github.com/mhx83/kanbas-react-web-app">GitHub Repository</a></p>
