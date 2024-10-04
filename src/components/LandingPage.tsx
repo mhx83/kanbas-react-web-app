07b3206ffe00c0c21825ac872eeb2e8d9447cc46
Minghong Xia <152064456+mhx83@users.noreply.github.com>
2024-10-03 17:04:25 -0700

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -6,7 +6,6 @@ function LandingPage() {
         <div>
             <h1>Minghong Xia - Section #01</h1>
             <ul>
-                <li><Link to="/Labs">Labs</Link></li>
                 <li><Link to="/Labs/Lab1">Lab 1</Link></li>
                 <li><Link to="/Labs/Lab2">Lab 2</Link></li>
                 <li><Link to="/Labs/Lab3">Lab 3</Link></li>
